b118c63412a7a06c53ee521c728e98f742f723cf
Sodiqmirzo Sattorov <mr.sodiqmirzosattorov17@gmail.com>
2022-07-05 18:03:17 +0500

    added fields in transfer history dto

--- src/Trait/Base.php
+++ src/Trait/Base.php
@@ -26,19 +26,20 @@ trait Base
             'Authorization' => Base64::encode($this->config['svgate_login'] . ':' . $this->config['svgate_password']),
         ])->post($url, $preparedParams)
             ->throw(function ($response, $e) {
-                match ($response->status()) {
-                    -200 => throw new Exception('Card not found', -200),
-                    -201 => throw new Exception('Card expired, capture card!', -201),
-                    -202 => throw new Exception('Pan invalid, wrong format!', -202),
-                    -100 => throw new Exception('Empty parameters!', -100),
-                    -101 => throw new Exception('Error while executing the request, try again!', -101),
-                    -102 => throw new Exception('Error while executing the request, try again!', -102),
-                    -103 => throw new Exception('Checkout required fields!', -103),
-                    -199 => throw new Exception('JsonRpc ERROR', -199),
-                    -203 => throw new Exception('Card not re-issue!', -203),
-                    -204 => throw new Exception('Card status is not active!', -204),
-                    -205 => throw new Exception('Card blocked!', -205),
-                };
+                throw new Exception($response->getBody()->getContents(), $response->status());
+//                match ($response->status()) {
+//                    -200 => throw new Exception('Card not found', -200),
+//                    -201 => throw new Exception('Card expired, capture card!', -201),
+//                    -202 => throw new Exception('Pan invalid, wrong format!', -202),
+//                    -100 => throw new Exception('Empty parameters!', -100),
+//                    -101 => throw new Exception('Error while executing the request, try again!', -101),
+//                    -102 => throw new Exception('Error while executing the request, try again!', -102),
+//                    -103 => throw new Exception('Checkout required fields!', -103),
+//                    -199 => throw new Exception('JsonRpc ERROR', -199),
+//                    -203 => throw new Exception('Card not re-issue!', -203),
+//                    -204 => throw new Exception('Card status is not active!', -204),
+//                    -205 => throw new Exception('Card blocked!', -205),
+//                };
             })
             ->json('result');
     }
